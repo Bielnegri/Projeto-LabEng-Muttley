61931ae7ed0e17346f0e0d29daf22c91cdfa68c7
Gabrielnegri <gabriel.benedito.negri@outlook.com>
2026-03-18 22:40:46 -0300

    Criação da entidade AulaAberta e suas demais classes relacionadas.

---
+++ Muttley/src/main/java/com/fatec/Muttley/aulaAberta/AulaAberta.java
@@ -0,0 +1,61 @@
+package com.fatec.Muttley.aulaAberta;
+
+import java.sql.Date;
+
+import jakarta.persistence.Id;
+
+public class AulaAberta {
+    @Id
+    private long id;
+    private String tema;
+    private String local;
+    private Date data;
+    private String horario;
+
+    public AulaAberta() {
+        super();
+    }
+    public AulaAberta(String tema, String local, Date data, String horario) {
+        this.tema = tema;
+        this.local = local;
+        this.data = data;
+        this.horario = horario;
+    }
+
+    public long getId() {
+        return id;
+    }
+    public String getTema() {
+        return tema;
+    }
+    public String getLocal() {
+        return local;
+    }
+    public Date getData() {
+        return data;
+    }
+    public String getHorario() {
+        return horario;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+    public void setTema(String tema) {
+        this.tema = tema;
+    }
+    public void setLocal(String local) {
+        this.local = local;
+    }
+    public void setData(Date data) {
+        this.data = data;
+    }
+    public void setHorario(String horario) {
+        this.horario = horario;
+    }
+
+    @Override
+    public String toString() {
+        return tema + "\n" + local + "\n" + horario;
+    }
+}
---
+++ Muttley/src/main/java/com/fatec/Muttley/aulaAberta/AulaAbertaRepository.java
@@ -0,0 +1,5 @@
+package com.fatec.Muttley.aulaAberta;
+
+public class AulaAbertaRepository {
+    
+}
