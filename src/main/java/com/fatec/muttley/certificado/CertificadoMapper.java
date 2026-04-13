289ec5769280d9df4b3c0c7af6fe322a60cd0237
João Pedro <jplhfernandez@gmail.com>
2026-04-13 17:33:48 -0300

    Terminando CertificadoController resta: salvar, deletarCertificado

--- src/main/java/com/fatec/muttley/certificado/CertificadoMapper.java
+++ src/main/java/com/fatec/muttley/certificado/CertificadoMapper.java
@@ -1,7 +1,16 @@
 package com.fatec.muttley.certificado;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface CertificadoMapper {
+    AtualizacaoCertificado toAtualizacaoDto(Certificado certificado);
+
+    @Mapping(target = "id", ignore = true)
+    Certificado toEntityFromAtualizacao(AtualizacaoCertificado dto);
+
+    @Mapping(target = "id", ignore = true)
+    void updateEntityFromDto(AtualizacaoCertificado dto, @MappingTarget Certificado certificado);
 }
--- src/main/java/com/fatec/muttley/certificado/CertificadoRepository.java
+++ src/main/java/com/fatec/muttley/certificado/CertificadoRepository.java
@@ -1,5 +1,11 @@
 package com.fatec.muttley.certificado;
 
-public class CertificadoRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+@Transactional
+public interface CertificadoRepository extends JpaRepository<Certificado, Long> {
     
 }
