51fc0adcfb1f7f203a0b90e946071d794e370333
João Pedro <jplhfernandez@gmail.com>
2026-04-14 13:04:40 -0300

    Substituindo comentário Caminhão por Medalha

--- src/main/java/com/fatec/muttley/medalha/MedalhaController.java
+++ src/main/java/com/fatec/muttley/medalha/MedalhaController.java
@@ -34,7 +34,7 @@ public class MedalhaController {
         if (id != null) {
             //edição: Carrega dados existentes
             Medalha medalha = medalhaService.procurarPorId(id)
-                    .orElseThrow(() -> new EntityNotFoundException("Caminhão não encontrado"));
+                    .orElseThrow(() -> new EntityNotFoundException("Medalha não encontrado"));
             dto = medalhaMapper.toAtualizacaoDto(medalha);
         } else {
             // criação: DTO vazio
