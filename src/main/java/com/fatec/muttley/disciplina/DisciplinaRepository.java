68c9c3d81370c27766900f511683ac20a97e1447
João Pedro <jplhfernandez@gmail.com>
2026-04-14 10:37:23 -0300

    DisciplinaRepository feito

--- src/main/java/com/fatec/muttley/disciplina/DisciplinaRepository.java
+++ src/main/java/com/fatec/muttley/disciplina/DisciplinaRepository.java
@@ -1,5 +1,11 @@
 package com.fatec.muttley.disciplina;
 
-public interface DisciplinaRepository {
-    
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Transactional
+public interface DisciplinaRepository extends JpaRepository<Disciplina, Long> {
+
 }
