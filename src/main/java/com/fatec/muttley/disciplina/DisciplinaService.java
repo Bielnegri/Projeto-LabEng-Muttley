15a72b6d27e82e18aac49b9b9e4d67f3805f79e7
João Pedro <jplhfernandez@gmail.com>
2026-04-14 12:37:36 -0300

    DisciplinaService feito

--- src/main/java/com/fatec/muttley/disciplina/DisciplinaService.java
+++ src/main/java/com/fatec/muttley/disciplina/DisciplinaService.java
@@ -1,4 +1,44 @@
 package com.fatec.muttley.disciplina;
 
+import com.fatec.muttley.certificado.CertificadoRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class DisciplinaService {
+    @Autowired
+    private DisciplinaRepository disciplinaRepository;
+    @Autowired
+    private DisciplinaMapper disciplinaMapper;
+    @Autowired
+    private CertificadoRepository certificadoRepository;
+
+    public Disciplina salvarOuAtualizar(AtualizacaoDisciplina dto){
+        if (dto.id() != null){
+            Disciplina existente = disciplinaRepository.findById(dto.id())
+                    .orElseThrow(() -> new EntityNotFoundException("Disciplina não encontrada com id: " + dto.id()));
+            disciplinaMapper.updateEntityFromDto(dto, existente);
+            return disciplinaRepository.save(existente);
+        } else {
+            Disciplina novaDisciplina = disciplinaMapper.toEntityFromAtualizacao(dto);
+            return disciplinaRepository.save(novaDisciplina);
+        }
+    }
+
+    public List<Disciplina> procurarTodas(){
+        return disciplinaRepository.findAll(Sort.by("nome").ascending());
+    }
+
+    public void apagarPorId(Long id){
+        certificadoRepository.deleteById(id);
+    }
+
+    public Optional<Disciplina> procurarPorId(Long id){
+        return disciplinaRepository.findById(id);
+    }
 }
