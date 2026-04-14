39ae36e7bc3c3084cef33984bcd07d433adb56e0
João Pedro <jplhfernandez@gmail.com>
2026-04-14 10:35:42 -0300

    DisciplinaMapper feito

--- src/main/java/com/fatec/muttley/disciplina/DisciplinaMapper.java
+++ src/main/java/com/fatec/muttley/disciplina/DisciplinaMapper.java
@@ -1,4 +1,16 @@
 package com.fatec.muttley.disciplina;
 
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+
+@Mapper(componentModel = "spring")
 public interface DisciplinaMapper {
+    AtualizacaoDisciplina toAtualizacaoDto(Disciplina disciplina);
+
+    @Mapping(target = "id", ignore = true)
+    Disciplina toEntityFromAtualizacao(AtualizacaoDisciplina dto);
+
+    @Mapping(target = "id", ignore = true)
+    void updateEntityFromDto(AtualizacaoDisciplina dto, @MappingTarget Disciplina disciplina);
 }
