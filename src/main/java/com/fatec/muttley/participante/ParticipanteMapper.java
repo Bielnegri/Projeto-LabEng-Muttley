edd57c7bedb2f6ddfca4481f18c509f09e8657a7
andre <andreolamego@gmail.com>
2026-04-18 00:30:11 -0300

    :bug: fix: corrige fluxo de participante para incluir os novos relacionamentos (`Evento` e `Aluno`)

--- src/main/java/com/fatec/muttley/participante/ParticipanteMapper.java
+++ src/main/java/com/fatec/muttley/participante/ParticipanteMapper.java
@@ -7,11 +7,17 @@ import org.mapstruct.MappingTarget;
 @Mapper(componentModel = "spring")
 public interface ParticipanteMapper {
 
+    @Mapping(target = "alunoId", source = "aluno.id")
+    @Mapping(target = "eventoId", source = "evento.id")
     AtualizacaoParticipante toAtualizacaoDto(Participante participante);
 
     @Mapping(target = "id", ignore = true)
+    @Mapping(target = "aluno", ignore = true)
+    @Mapping(target = "evento", ignore = true)
     Participante toEntityFromAtualizacao(AtualizacaoParticipante dto);
 
     @Mapping(target = "id", ignore = true)
+    @Mapping(target = "aluno", ignore = true)
+    @Mapping(target = "evento", ignore = true)
     void updateEntityFromDto(AtualizacaoParticipante dto, @MappingTarget Participante participante);
 }
