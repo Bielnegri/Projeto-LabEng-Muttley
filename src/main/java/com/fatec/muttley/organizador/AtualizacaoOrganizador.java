e529f0d971e233c9a92a418487e3944987d58443
Gabrielnegri <gabriel.benedito.negri@outlook.com>
2026-05-04 19:17:06 -0300

    :bug: fix: correção das anotações nos DTOs de `Aluno`, `Medalha`, `Organizador`, `Palestrante` e `Professor`

--- src/main/java/com/fatec/muttley/organizador/AtualizacaoOrganizador.java
+++ src/main/java/com/fatec/muttley/organizador/AtualizacaoOrganizador.java
@@ -1,12 +1,11 @@
 package com.fatec.muttley.organizador;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 
 public record AtualizacaoOrganizador(
         Long id,
 
-        @NotNull(message = "Instituição é obrigatória")
+        @NotBlank(message = "Instituição é obrigatória")
         String instituicao,
 
         @NotBlank(message = "Cargo é obrigatório")
--- src/main/java/com/fatec/muttley/professor/AtualizacaoProfessor.java
+++ src/main/java/com/fatec/muttley/professor/AtualizacaoProfessor.java
@@ -1,12 +1,11 @@
 package com.fatec.muttley.professor;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 
 public record AtualizacaoProfessor(
         Long id,
 
-        @NotNull(message = "Área de formação é obrigatória")
+        @NotBlank(message = "Área de formação é obrigatória")
         String areaFormacao,
 
         @NotBlank(message = "Titulação é obrigatória")
