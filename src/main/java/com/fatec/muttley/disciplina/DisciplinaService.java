c5498fb68a08a32110bdab9ba77202eb60be0ea5
andre <andreolamego@gmail.com>
2026-05-06 23:31:23 -0300

    :sparkles: feat: adiciona gerenciamento de disciplinas no painel administrativo

--- src/main/java/com/fatec/muttley/disciplina/DisciplinaService.java
+++ src/main/java/com/fatec/muttley/disciplina/DisciplinaService.java
@@ -14,38 +14,42 @@ import java.util.Optional;
 public class DisciplinaService {
     @Autowired
     private DisciplinaRepository disciplinaRepository;
+
     @Autowired
     private ProfessorRepository professorRepository;
+
     @Autowired
     private DisciplinaMapper disciplinaMapper;
 
-    public Disciplina salvarOuAtualizar(AtualizacaoDisciplina dto){
+    public Disciplina salvarOuAtualizar(AtualizacaoDisciplina dto) {
+        Professor professor = null;
+        if (dto.id_professor() != null) {
+            professor = professorRepository.findById(dto.id_professor())
+                    .orElseThrow(() -> new EntityNotFoundException("Professor nao encontrado com id: " + dto.id_professor()));
+        }
+
         Disciplina disciplina;
-        if (dto.id() != null){
-            disciplina  = disciplinaRepository.findById(dto.id())
-                    .orElseThrow(() -> new EntityNotFoundException("Disciplina não encontrada com id: " + dto.id()));
+        if (dto.id() != null) {
+            disciplina = disciplinaRepository.findById(dto.id())
+                    .orElseThrow(() -> new EntityNotFoundException("Disciplina nao encontrada com id: " + dto.id()));
             disciplinaMapper.updateEntityFromDto(dto, disciplina);
-            return disciplinaRepository.save(disciplina);
         } else {
             disciplina = disciplinaMapper.toEntityFromAtualizacao(dto);
         }
-        if (dto.id_professor() != null) {
-            Professor professor = professorRepository.findById(dto.id_professor())
-                    .orElseThrow(() -> new EntityNotFoundException("Professor não encontrado com id: " + dto.id_professor()));
-            disciplina.setProfessor(professor);
-        }
+
+        disciplina.setProfessor(professor);
         return disciplinaRepository.save(disciplina);
     }
 
-    public List<Disciplina> procurarTodas(){
+    public List<Disciplina> procurarTodas() {
         return disciplinaRepository.findAll(Sort.by("nome").ascending());
     }
 
-    public void apagarPorId(Long id){
+    public void apagarPorId(Long id) {
         disciplinaRepository.deleteById(id);
     }
 
-    public Optional<Disciplina> procurarPorId(Long id){
+    public Optional<Disciplina> procurarPorId(Long id) {
         return disciplinaRepository.findById(id);
     }
 }
