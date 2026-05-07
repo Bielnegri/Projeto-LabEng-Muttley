efd4730f9e8c24ec4bd581dada554d562868ae97
Hiiroshi <hiroshixdyu@gmail.com>
2026-05-07 16:57:29 -0300

    :sparkles: feat: API Email

--- src/main/java/com/fatec/muttley/AdminController.java
+++ src/main/java/com/fatec/muttley/AdminController.java
@@ -5,7 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import com.fatec.muttley.email.GmailService;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 
 @Controller
 @RequestMapping("/admin")
@@ -19,5 +23,20 @@ public class AdminController {
         model.addAttribute("proximosEventos", eventoService.procurarTodosOrdenarPorInicio());
         return "admin/inicio";
     }
-
 }
+
+//    @PostMapping("/testeEmail")
+//    public String testeEmail(RedirectAttributes redirectAttributes) {
+//        try {
+//            GmailService.enviarEmail(
+//                    "gabrielbnegri@gmail.com",
+//                    "Envio automatico do Muttley",
+//                    "ATENÇÃO, ESTE EMAIL FOI ENVIADO AUTOMATICAMENTE"
+//            );
+//            redirectAttributes.addFlashAttribute("sucesso", "E-mail enviado!");
+//        } catch (Exception e) {
+//            redirectAttributes.addFlashAttribute("erro", "Erro: " + e.getMessage());
+//        }
+//        return "redirect:/admin/inicio";
+//    }
+//}
